86b4e916ca11f4ee4d65c9d74f5b252d2c7adf6c
aceofdiamonds4500 <tacoparrot4500@gmail.com>
2025-05-13 03:30:19 -0400

    Add second perspective

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# 3D_with_SDL
+# 3D With SDL
 Experimenting with different ideas, all of which are 3D related
+
+## What I have so far
+What I'm experimenting with right now is how DOOM renders its graphics, and so far
+I have two view modes (top down perspective with player around world or world around
+player movement)
+
+## Controls
+- Classic DOOM style controls
+  - `W` - Forward
+  - `S` - Backward
+  - `A` - Strafe left
+  - `D` - Strafe right
+  - `Left and right` - Turn left and right
+
+## TO-DO
+I need to work on my first-person perspective and my third-person map that moves
+the world around the player's view.
+
+I wrote the world-around-player perspective in a really hacky way but it's 3 am and I'm done with it for now.
+
+## Building
+
+- navigate to `/build`
+- type `make` in the console
+- run `./GameEngine`
+
+lol idk how you're gonna use this if you're on windows, good luck (jk i'll figure it out)
--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-/*
- * This example creates an SDL window and renderer, and then draws some lines
- * to it every frame.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
-
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#define WIDTH 640
+#define HEIGHT 480
+#define MID_X WIDTH / 2
+#define MID_Y HEIGHT / 2
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <stdio.h>
@@ -21,10 +19,12 @@ typedef struct {
 } Character;
 
 Character player;
+int view_mode = 1;
 
-/* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-{
+int xa = 240; int xb = 400;
+int ya = 450; int yb = 356;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Example Renderer Lines", "1.0", "com.example.renderer-lines");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -39,10 +39,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     player.x = 320.0;
     player.y = 240.0;
     player.angle = 0.0;
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
     switch (event->type) {
@@ -51,9 +50,15 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
         case SDL_EVENT_KEY_DOWN:
             SDL_Keycode sym = event->key.key;
             if (sym == SDLK_W){ player.x = player.x + SDL_cos(player.angle); player.y = player.y + SDL_sin(player.angle);}
-            if (sym == SDLK_A){ player.angle = player.angle + 0.1;}
+            if (sym == SDLK_A){ player.x = player.x + SDL_sin(player.angle); player.y = player.y - SDL_cos(player.angle);}
             if (sym == SDLK_S){ player.x = player.x - SDL_cos(player.angle); player.y = player.y - SDL_sin(player.angle);}
-            if (sym == SDLK_D){ player.angle = player.angle - 0.1;}
+            if (sym == SDLK_D){ player.x = player.x - SDL_sin(player.angle); player.y = player.y + SDL_cos(player.angle);}
+            if (sym == SDLK_LEFT){ player.angle = player.angle + 0.1;}
+            if (sym == SDLK_RIGHT){ player.angle = player.angle - 0.1;}
+            if (sym == SDLK_M) {
+                view_mode++;
+                if (view_mode > 3){ view_mode = 1; }
+            }
             break;
         case SDL_EVENT_KEY_UP:
 
@@ -61,54 +66,120 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
         default:
             break;
     }
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    int i;
-
-    /* Lines (line segments, really) are drawn in terms of points: a set of
-       X and Y coordinates, one set for each end of the line.
-       (0, 0) is the top left of the window, and larger numbers go down
-       and to the right. This isn't how geometry works, but this is pretty
-       standard in 2D graphics. */
+    // static const SDL_FPoint line_points1[] = {
+    //     { 310,240 }, { 330, 240 },
+    //     { 325,235 }, {330,240 }, {325,245 }
+    // };
     static const SDL_FPoint line_points[] = {
-        { 100, 354 }, { 220, 230 }, { 140, 230 }, { 320, 100 }, { 500, 230 },
-        { 420, 230 }, { 540, 354 }, { 400, 354 }, { 100, 354 }
+        { 240, 450 }, { 400, 450 },{ 400, 356 },
+        { 240, 356 }, { 240, 450 }
     };
-    static const SDL_FPoint line_points1[] = {
-        { 310,240 }, { 330, 240 },
-        { 325,235 }, {330,240 }, {325,245 }
-    };
-    //printf("%f %f %f\n", player.x, player.y, player.angle);
-    /* as you can see from this, rendering draws over whatever was drawn before it. */
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black */
-    SDL_RenderClear(renderer);  /* start with a blank canvas. */
+    SDL_RenderClear(renderer);
+    int vx1 = xa;
+    int vy1 = ya;
+    int vx2 = xb;
+    int vy2 = yb;
 
-    /* You can draw lines, one at a time, like these brown ones... */
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-    SDL_RenderLine(renderer, 240, 450, 400, 450);
-    SDL_RenderLine(renderer, 240, 356, 400, 356);
-    SDL_RenderLine(renderer, 240, 356, 240, 450);
-    SDL_RenderLine(renderer, 400, 356, 400, 450);
+    double tx1 = vx1 - player.x; double ty1 = vy1 - player.y;
+    double tx2 = vx2 - player.x; double ty2 = vy2 - player.y;
 
-    /* You can also draw a series of connected lines in a single batch... */
-    //SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
-    //SDL_RenderLines(renderer, line_points1, SDL_arraysize(line_points1));
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderLine(renderer, player.x,player.y , SDL_cos(player.angle)*20 + player.x,SDL_sin(player.angle)*20 + player.y);
+    double tz1 = tx1 * SDL_cos(player.angle) + ty1 * SDL_sin(player.angle);
+    double tz2 = tx2 * SDL_cos(player.angle) + ty2 * SDL_sin(player.angle);
+    tx1 = tx1 * SDL_sin(player.angle) - ty1 * SDL_cos(player.angle);
+    tx2 = tx2 * SDL_sin(player.angle) - ty2 * SDL_cos(player.angle);
 
+    if (view_mode == 1) {
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderLine(renderer, player.x,player.y , SDL_cos(player.angle)*20 + player.x,SDL_sin(player.angle)*20 + player.y);
 
-    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderLine(renderer, xa, ya, xb, yb);
+        SDL_RenderLine(renderer, xa, ya, xb, ya);
+        SDL_RenderLine(renderer, xa, yb, xb, yb);
+        SDL_RenderLine(renderer, xa, yb, xa, ya);
+        SDL_RenderLine(renderer, xb, yb, xb, ya);
+    }
+    if (view_mode == 2) {
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderLine(renderer, MID_X,MID_Y , MID_X,MID_Y-20);
+//320 - tx1, 240 - tz1, 320 - tx2, 240 - tz1
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderLine(renderer, 320 - tx1, 240 - tz1, 320 - tx2, 240 - tz2);
 
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+        vx1 = xa;
+        vy1 = ya;
+        vx2 = xb;
+        vy2 = ya;
+
+        tx1 = vx1 - player.x; ty1 = vy1 - player.y;
+        tx2 = vx2 - player.x; ty2 = vy2 - player.y;
+
+        tz1 = tx1 * SDL_cos(player.angle) + ty1 * SDL_sin(player.angle);
+        tz2 = tx2 * SDL_cos(player.angle) + ty2 * SDL_sin(player.angle);
+        tx1 = tx1 * SDL_sin(player.angle) - ty1 * SDL_cos(player.angle);
+        tx2 = tx2 * SDL_sin(player.angle) - ty2 * SDL_cos(player.angle);
+        SDL_RenderLine(renderer, 320 - tx1, 240 - tz1, 320 - tx2, 240 - tz2);
+
+        vx1 = xa;
+        vy1 = yb;
+        vx2 = xb;
+        vy2 = yb;
+
+        tx1 = vx1 - player.x; ty1 = vy1 - player.y;
+        tx2 = vx2 - player.x; ty2 = vy2 - player.y;
+
+        tz1 = tx1 * SDL_cos(player.angle) + ty1 * SDL_sin(player.angle);
+        tz2 = tx2 * SDL_cos(player.angle) + ty2 * SDL_sin(player.angle);
+        tx1 = tx1 * SDL_sin(player.angle) - ty1 * SDL_cos(player.angle);
+        tx2 = tx2 * SDL_sin(player.angle) - ty2 * SDL_cos(player.angle);
+        SDL_RenderLine(renderer, 320 - tx1, 240 - tz1, 320 - tx2, 240 - tz2);
+
+        vx1 = xa;
+        vy1 = yb;
+        vx2 = xa;
+        vy2 = ya;
+
+        tx1 = vx1 - player.x; ty1 = vy1 - player.y;
+        tx2 = vx2 - player.x; ty2 = vy2 - player.y;
+
+        tz1 = tx1 * SDL_cos(player.angle) + ty1 * SDL_sin(player.angle);
+        tz2 = tx2 * SDL_cos(player.angle) + ty2 * SDL_sin(player.angle);
+        tx1 = tx1 * SDL_sin(player.angle) - ty1 * SDL_cos(player.angle);
+        tx2 = tx2 * SDL_sin(player.angle) - ty2 * SDL_cos(player.angle);
+        SDL_RenderLine(renderer, 320 - tx1, 240 - tz1, 320 - tx2, 240 - tz2);
+
+        vx1 = xb;
+        vy1 = yb;
+        vx2 = xb;
+        vy2 = ya;
+
+        tx1 = vx1 - player.x; ty1 = vy1 - player.y;
+        tx2 = vx2 - player.x; ty2 = vy2 - player.y;
+
+        tz1 = tx1 * SDL_cos(player.angle) + ty1 * SDL_sin(player.angle);
+        tz2 = tx2 * SDL_cos(player.angle) + ty2 * SDL_sin(player.angle);
+        tx1 = tx1 * SDL_sin(player.angle) - ty1 * SDL_cos(player.angle);
+        tx2 = tx2 * SDL_sin(player.angle) - ty2 * SDL_cos(player.angle);
+        SDL_RenderLine(renderer, 320 - tx1, 240 - tz1, 320 - tx2, 240 - tz2);
+
+        // SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+        // SDL_RenderLines(renderer, line_points, SDL_arraysize(line_points));
+}
+    printf("%f %f %f %f\n", tx1, tz1, tx2, tz2);
+
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    /* SDL will clean up the window/renderer for us. */
 }
 
